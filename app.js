44c2154095fab19c1d689fc09b6810a42e8e47e3
Leila <leila.sida@outlook.com>
2024-03-20 13:43:03 +0100

    integration du DOM
    creation d un fichier app.js  contenant l ensemble des constantes permettant de rendre dynamique le fichier html

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 //DOM manejo de los componentes del html manejado por el javascript
 
-const container = document.querySelector
+const container = document.querySelector("container"); // contenedor principal
+const image = document.querySelector("#music-image"); // todos los elementos imagenes
+const title= document.querySelector("#music-details .title"); //titulo
+const singer= document.querySelector("#music-details .singer");//cantante
+
+// botones de accion
+
+const play= document.querySelector("#control #play");
+const prev= document.querySelector("#control #prev");
+const next= document.querySelector("#control #next");
+
+//progression de la musica
+
+const duration = document.querySelector("#duration");
+const currentTime = document.querySelector("#current-time");
+const progressBar = document.querySelector("#progress-bar");
+const volume = document.querySelector("#volume");
+const volumeBar = document.querySelector("#volume-bar");
+const ul = document.querySelector("#ul"); // lista de canciones
+
+
