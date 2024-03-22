def1318d8f365d255b7883a1e01d6cd7e137f07d
Leila <leila.sida@outlook.com>
2024-03-22 13:58:15 +0100

    event por la barra de control

--- app.js
+++ app.js
@@ -23,13 +23,14 @@ const ul = document.querySelector("#ul"); // lista de canciones
 //insttancia de la MusicPlayer
 
 const player = new MusicPlayer(musicList);
+
 // evento dentro del html ejecutar una ventana que me indique el program y las canciones se han cargado
 window.addEventListener("load",()=>{
     let music = player.getMusic(); // musica actual
     displayMusic(music); // Mostrar los detalles de la cancion
     displayMusicList(player.music);// Lista de reproduccion
     isPlayingNow(); //comprueba si hay musica reproduciendo
-})
+});
 
 function displayMusic(music){
     // titulo de la cancion
@@ -40,7 +41,7 @@ function displayMusic(music){
     image.src = "img/" + music.img;
     //archivo ogg, wav, mp3
     audio.src = "music/" 
-}
+};
 
 // Evento  para los botones reproduccion y pausa acciones predefinida (click, mouseover, mousedown)
 
@@ -58,24 +59,25 @@ prev.addEventListener("click",()=>{
 });
 
 next.addEventListener("click",()=>{
-    nextMusic()});
+    nextMusic()
+});
 
     //acciones que reproduzca la cancion correspondiente anterior
     const prevMusic = ()=>{
         player.prev();
-        let music = player.getMusic()
+        let music = player.getMusic();
         displayMusic(music);
         playMusic();
-        isPlayingNow();
+        isPlayingNow()
     };
 
     //acciones que reproduzca la cancion correspondiente siguiente
     const nextMusic = ()=>{
         player.next();
-        let music = player.getMusic()
+        let music = player.getMusic();
         displayMusic(music);
         playMusic();
-        isPlayingNow();
+        isPlayingNow()
     };
     
 
@@ -94,7 +96,7 @@ next.addEventListener("click",()=>{
     //volver activar el playing
     //asocialo a la etiqueta "i"
     //pasar de || a icono play
-    //
+    
     const playMusic = () => {container.classList.remove("playing");
     play.querySelector("i").classList = "fa-solid fa-play";
     audio.play};
@@ -113,10 +115,31 @@ next.addEventListener("click",()=>{
         const result = `${seconds}:${updateMinutes}`;
         return result
             
-    }
+    };
 
     //evento que calcula el tiempo de reproduccion y el tiempo de la cancion
     //"loadmetadata" => Accion
     //"addEventListener" => metodo event a escuchar
-    audio.addEventListener("loadadmetadata",()=>{/*codigo*/})
-    audio.addEventListener("timeupdate",()=>{/*codigo*/})
+    // tengo que solicitar la duracion del audio
+    //tengo que solicitar que se mueva junto con el tiempo de la cancion
+    audio.addEventListener("loadadmetadata",()=>{
+        duration.textContent=calculateTime(audio.duration)
+       progressBar.max=Math.floor(audio.duration);
+    });
+
+    //progresion de la barra
+    //calcul en la anterior funcion y me muestre el valor
+    audio.addEventListener("timeupdate",()=>{
+        progressBar.value=Math.floor(currentTime);
+        currentTime.textContent = calculateTime(progressBar.value)
+    });
+
+    //cuando mueva el boton de la barra de progreso se actualice el tiempo a la izquierda
+
+    progressBar.addEventListener("input",()=>{
+        currentTime.textContent =calculateTime(progressBar.value)
+        audio.currentTime = progressBar.value;
+    })
+    audio
+
+
